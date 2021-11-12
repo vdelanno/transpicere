a4451dd653d7745478f22e25c6aff4381a72f309
vincent <vincent.delannoy@gmail.com>
2021-11-12 23:07:23 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='transpicere',
+      version='0.1',
+      description='Python graphql modeler',
+      author='vincent delannoy',
+      author_email='vincent.delannoy@gmail.com',
+      url='https://github.com/vdelanno/transpicere',
+      packages=['transpicere'],
+      )
